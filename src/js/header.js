fa0d2219fb9f0585c124d7f7d5be87544cfd1ecb
prafessor <olegmilyo@gmail.com>
2026-01-20 17:18:09 +0200

    feat: add modal menu for mobile

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,10 @@
 const navBtn = document.querySelector(".navigation__btn");
 const navList = document.querySelector(".navigation__list");
 
+const modalMenuEl = document.querySelector(".modal-menu__overlay");
+const modalOpenBtn = document.querySelector(".menu-burger");
+const modalCloseBtn = document.querySelector(".modal-menu__btn-x");
+
 const onOutsideListClick = (evt) => {
   if(!navList.contains(evt.target)) {
     navList.classList.add("display-none");
@@ -12,10 +16,20 @@ const onOutsideListClick = (evt) => {
 const onNavBtnClick = (evt) => {
   evt.stopPropagation()
   const isHidden = navList.classList.toggle("display-none");
-  
+
   if(!isHidden) {
     document.addEventListener("click", onOutsideListClick);
   }
 }
 
 navBtn.addEventListener("click", onNavBtnClick);
+
+//  operation with modal menu
+
+modalOpenBtn.addEventListener("click", () => {
+  modalMenuEl.classList.add("open");
+})
+
+modalCloseBtn.addEventListener("click", () => {
+  modalMenuEl.classList.remove("open");
+})
