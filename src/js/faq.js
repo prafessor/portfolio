0027cc043400f3c8f7c3dd4874c372a6515572c5
prafessor <olegmilyo@gmail.com>
2026-01-23 09:35:03 +0200

    feat: add faq section and fix svg hrefs in benefits and about me sections

---
+++ src/js/faq.js
@@ -0,0 +1,5 @@
+import Accordion from 'accordion-js';
+
+new Accordion('.faq-accordion__container', {
+  showMultiple: true,
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import './header.js';
 import './about.js';
 import './projects.js';
+import './faq.js';
